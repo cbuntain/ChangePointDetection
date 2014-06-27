ebbfcba2df741777d1b7d7f6f8e693ee07447884
chrisnatoli <chrisnatoli@gmail.com>
2014-06-27 10:39:04 +0200

    changed simulated process from white noise to VAR(1)' some other cosmetic changes

--- simulated_data/simulate_data.py
+++ simulated_data/simulate_data.py
@@ -6,35 +6,38 @@ num_dimensions = 3
 total_time = 1000
 num_changepoints = 2
  
-all_data = []
+data = []
 covariances = []
 for i in range(num_changepoints + 1):
-    # Add an extra datapoint to the last set so there are
-    # total_time points overall.
     n = int(total_time / (num_changepoints + 1))
-    if i == num_changepoints:
-        n = n + 1
 
     # Generate a symmetric, positive semidefinite matrix.
     M = np.random.rand(num_dimensions, num_dimensions)
-    M = M + np.transpose(M) + num_dimensions * np.identity(num_dimensions)
-    M = M * 2**i
+    M = M + M.T + num_dimensions * np.identity(num_dimensions)
+    M = 3**i * M # Increase the covariance after each changepoint.
     np.linalg.cholesky(M) # Check that M is symmetric positive semidefinite.
 
-    # Generate n errors.
-    error_covariance = M
-    error_mean = np.array([0] * num_dimensions)
-    errors = np.random.multivariate_normal(error_mean, error_covariance, n)
+    # Generate n innovations.
+    innovation_covariance = M
+    innovation_mean = np.array([0] * num_dimensions)
+    innovations = np.random.multivariate_normal(innovation_mean,
+                                                innovation_covariance, n)
 
-    # Generate the data (currently just a white noise sequence).
-    data = errors
+    # Start the process with a draw from the distribution of innovations.
+    if data == []:
+        data.extend(np.random.multivariate_normal(innovation_mean,
+                                                  innovation_covariance, 1))
 
-    all_data.extend(data.tolist())
-    covariances.append(error_covariance)
+    # Generate the data (currently just VAR(1) process).
+    Phi = np.array([[0.6, 0.2, 0], [0.2, 0.4, 0], [0.6, 0.2, 0.5]])
+    for t in range(n):
+        datapoint = np.dot(Phi, data[(i*n) + t]) + innovations[t]
+        data.append(datapoint.tolist())
+    covariances.append(innovation_covariance)
 
 '''
 # Look at the time series of the first component.
-first_component = [ datapoint[0] for datapoint in all_data ]
+first_component = [ datapoint[0] for datapoint in data ]
 plt.plot(first_component)
 plt.show()
 '''
@@ -44,7 +47,7 @@ data_output = 'simulation_{}dim_{}changes_data.csv'.format(
     num_dimensions, num_changepoints)
 with open(data_output, 'w') as fp:
     writer = csv.writer(fp, delimiter=',')
-    writer.writerows(all_data)
+    writer.writerows(data)
 
 # Write the covariance matrices.
 covariance_output = 'simulation_{}dim_{}changes_covariances.csv'.format(
